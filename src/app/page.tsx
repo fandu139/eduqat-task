4a2f2a504ccb2cefdb72485545b43bc056b6b760
fandu139 <fandupratama@gmail.com>
2023-10-02 00:07:20 +0800

    implement add lesson materi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import Image from 'next/image'
 
 import IconMoveDot from "@/assets/icons/move-dot.svg";
+import IconPlus from "@/assets/icons/plus.svg";
 import IconPencil from "@/assets/icons/pencil.svg";
 
 import Text from '@/app/atom/Text'
@@ -62,7 +63,6 @@ export default function Home(this: any) {
   const grid = 8;
 
   const getListStyle = () => ({
-    padding: grid,
     overflow: 'auto',
   });
 
@@ -94,6 +94,54 @@ export default function Home(this: any) {
     setData(itemsManipulate)
   };
 
+  const handleAddSession = () => {
+    const dataLength = data.length + 1;
+    const dataPush = [
+      ...data,
+      {
+        id: `item-${dataLength}`,
+        name: `Session ${dataLength}`,
+        lessonMaterial: [],
+      }
+    ];
+    setData(dataPush)
+  }
+
+  const handleAddLesson = (index: number) => {
+    const dataLength = data[index].lessonMaterial.length + 1;
+
+    const manipulateLessonMaterial = {
+      ...data[index],
+      lessonMaterial: [
+        ...data[index].lessonMaterial,
+        {
+          id: `${dataLength}`,
+          title: `Materi ${dataLength}`,
+          type: "video",
+          date: "2023-09-22T16:48",
+          duration: 30,
+        },
+      ]
+    }
+
+    const _data = [...data];
+    _data.splice(index, 1, manipulateLessonMaterial);
+    setData(_data)
+  }
+
+  const handleMoveLesson = (id, value) => {
+    console.log('fandu', id, value);
+    const _data = [...data];
+
+    const dataManipulate = {
+      ...data[id],
+      lessonMaterial: value
+    };
+
+    _data.splice(id, 1, dataManipulate);
+    setData(_data)
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Header title='Event' height='75' />
@@ -101,63 +149,78 @@ export default function Home(this: any) {
         <Card padding={4}>
           <Text>Event Schedule: 24 Oktober 2021, 16:30</Text>
         </Card>
-        <Card padding={4} margin='mt-4'>
-          <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="droppable" direction="horizontal">
-              {(provided, snapshot) => (
-                <div
-                  ref={provided.innerRef}
-                  style={getListStyle()}
-                  {...provided.droppableProps}
-                >
-                  {data.map((item, index) => {
-                    return (
-                      <Draggable key={item.id} draggableId={item.id} index={index}>
-                        {(provided) => (
-                          <div
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            style={getItemStyle(
-                              provided.draggableProps.style
-                            )}
-                          >
-                            <div className="d-flex flex-row">
-                              <div className="p-2">
-                                <Image
-                                  src={IconMoveDot}
-                                  alt="Icon Move"
-                                  className="dark:invert"
-                                  width={40}
-                                  height={24}
-                                  priority
-                                />
-                              </div>
-                              <div className="p-2"><Text>{item.name}</Text></div>
-                              <div className="p-2">
-                                <Image
-                                  src={IconPencil}
-                                  alt="Icon Pencil"
-                                  className="dark:invert"
-                                  width={40}
-                                  height={24}
-                                  priority
-                                />
-                              </div>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="session">
+            {(provided) => (
+              <div
+                ref={provided.innerRef}
+                style={getListStyle()}
+                {...provided.droppableProps}
+              >
+                {data.map((item, index) => {
+                  return (
+                    <Draggable key={item.id} draggableId={item.id} index={index}>
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          style={getItemStyle(
+                            provided.draggableProps.style
+                          )}
+                          className={`border rounded p-4 mt-4`}
+                        >
+                          <div className="d-flex flex-row">
+                            <div
+                              className="p-2"
+                              {...provided.dragHandleProps}
+                            >
+                              <Image
+                                src={IconMoveDot}
+                                alt="Icon Move"
+                                className="dark:invert"
+                                width={40}
+                                height={24}
+                                priority
+                              />
                             </div>
-                            <MateriComponent />
+                            <div className="p-2"><Text>{item.name}</Text></div>
+                            <div className="p-2">
+                              <Image
+                                src={IconPencil}
+                                alt="Icon Pencil"
+                                className="dark:invert"
+                                width={40}
+                                height={24}
+                                priority
+                              />
+                            </div>
                           </div>
-                        )}
-                      </Draggable>
-                    )
-                  })}
-                </div>
-              )}
-            </Droppable>
-          </DragDropContext>
-        </Card>
-      </div>
-      <button className="btn btn-primary m-3">Button Primary</button>
-    </main>
+                          <MateriComponent id={index} dataLesson={item.lessonMaterial} handleMoveLesson={handleMoveLesson} />
+                          <div className="d-flex flex-row">
+                            <div className="p-2 btn btn-primary" onClick={() => handleAddLesson(index)}>
+                              <Image
+                                src={IconPlus}
+                                alt="Icon Move"
+                                className="dark:invert"
+                                width={20}
+                                height={20}
+                                priority
+                              />
+                            </div>
+                            <div className="p-2"><Text>Add Lesson Material</Text></div>
+                          </div>
+                        </div>
+                      )}
+                    </Draggable>
+                  )
+                })}
+                  {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+         <button className="btn btn-primary" onClick={handleAddSession}>Add Session</button>
+       </div>
+     </main>
   )
 }
